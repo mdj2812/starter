6ed6b10fb79c5347d624373fbd4f9688d930de8c
dima <martin.1.ma@nokia-sbell.com>
2024-04-02 02:15:32 +0000

    Enable spellcheck

--- lua/options.lua
+++ lua/options.lua
@@ -3,4 +3,6 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt = "both" -- to enable cursorline!
+o.cursorlineopt = "both"
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en"
